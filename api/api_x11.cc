832a89a5f462677ca64e153a6a33f8cc8fb4b792
Stephen White <senorblanco@chromium.org>
2025-04-17 12:46:12 -0400

    Use uint32_t for event modifier math in api.

--- api/api_x11.cc
+++ api/api_x11.cc
@@ -45,8 +45,8 @@ namespace Toucan {
 namespace {
 int ToToucanEventModifiers(int state) {
   int result = 0;
-  if (state & ShiftMask) { result |= static_cast<int>(EventModifiers::Shift); }
-  if (state & ControlMask) { result |= static_cast<int>(EventModifiers::Control); }
+  if (state & ShiftMask) { result |= static_cast<uint32_t>(EventModifiers::Shift); }
+  if (state & ControlMask) { result |= static_cast<uint32_t>(EventModifiers::Control); }
   return result;
 }
 }  // namespace
