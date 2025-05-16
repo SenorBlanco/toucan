83aa361542030f81abcc8aa22517130bd8036a3f
Stephen White <senorblanco@chromium.org>
2025-05-16 11:54:09 -0400

    Cleanup, implement pow() in LLVM.

--- codegen/codegen_llvm.cc
+++ codegen/codegen_llvm.cc
@@ -500,7 +500,8 @@ llvm::Intrinsic::ID CodeGenLLVM::FindIntrinsic(Method* method) {
       "Math", "cos",  llvm::Intrinsic::cos,     "Math", "fabs",  llvm::Intrinsic::fabs,
       "Math", "clz",  llvm::Intrinsic::ctlz,    "Math", "floor", llvm::Intrinsic::floor,
       "Math", "ceil", llvm::Intrinsic::ceil,    "Math", "tan",   llvm::Intrinsic::tan,
-      "Math", "min",  llvm::Intrinsic::minimum, "Math", "max",   llvm::Intrinsic::maximum
+      "Math", "min",  llvm::Intrinsic::minimum, "Math", "max",   llvm::Intrinsic::maximum,
+      "Math", "pow",  llvm::Intrinsic::pow
   };
 
   for (auto intrinsic : intrinsics) {
