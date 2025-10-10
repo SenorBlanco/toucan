ab9ccf6f3284b9ec15d10d614b0d59a7b92ed6f4
Stephen White <senorblanco@chromium.org>
2025-10-10 17:32:44 -0400

    Create .ipa files for ios.
    Make make-app.py responsible for code signing on iOS, including
    creating an entitlements file.
    Implement make-ipa.py. This is responsible for creating an .ipa file
    out of an .app file.

--- tools/make-app.py
+++ tools/make-app.py
@@ -16,21 +16,21 @@
 
 import argparse
 import os
+import plistlib
 import shutil
 import subprocess
-import sys
 import tempfile
 
 argparser = argparse.ArgumentParser(description="Create an App")
 argparser.add_argument('--target-name')
 argparser.add_argument('--target-os')
-argparser.add_argument('--out-dir')
 argparser.add_argument('--mobile-provision')
+argparser.add_argument('--team-identifier')
+argparser.add_argument('--codesign-identity')
 args = vars(argparser.parse_args())
 
 target_name = args['target_name']
 target_os = args['target_os']
-out_dir = os.path.abspath(args['out_dir'])
 
 info_plist = '''<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -46,8 +46,7 @@ info_plist = '''<?xml version="1.0" encoding="UTF-8"?>
 </plist>
 '''
 
-source_path = out_dir + "/"
-dest_app_path = source_path + target_name + ".app/"
+dest_app_path = target_name + ".app/"
 if target_os == "mac":
   dest_contents_path = dest_app_path + "Contents/"
   dest_os_path = dest_contents_path + "MacOS/"
@@ -66,11 +65,9 @@ if target_os == "mac":
     "libwebgpu_dawn.dylib",
   ]
   for dylib in dylibs:
-    source_lib = source_path + dylib
-    dest_lib = dest_os_path + dylib
-    shutil.copy2(source_lib, dest_lib)
+    shutil.copy2(dylib, dest_os_path + dylib)
 
-shutil.copy2(source_path + target_name, dest_os_path + target_name)
+shutil.copy2(target_name, dest_os_path + target_name)
 
 info_plist_file = dest_contents_path + "Info.plist"
 with open(info_plist_file, "w") as f:
@@ -78,5 +75,20 @@ with open(info_plist_file, "w") as f:
   f.close()
 
 if target_os == "ios":
+  team_identifier = args['team_identifier']
+  codesign_identity = args['codesign_identity']
   mobile_provision = os.path.abspath(args['mobile_provision'])
   shutil.copy2(mobile_provision, dest_app_path + '/' + 'embedded.mobileprovision')
+
+  (entitlements_file, entitlements_filename) = tempfile.mkstemp()
+  entitlements = dict([
+    ('application-identifier', team_identifier + ".org.toucanlang.sample.window"),
+    ('com.apple.developer.team-identifier', team_identifier),
+  ])
+
+  with open(entitlements_filename, "wb") as fp:
+    plistlib.dump(entitlements, fp, fmt=plistlib.FMT_XML)
+    fp.close()
+
+  subprocess.check_call(["codesign", "-s", codesign_identity, "--entitlements", entitlements_filename, dest_os_path])
+  os.remove(entitlements_filename)
